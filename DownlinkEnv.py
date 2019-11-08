04e5c7384b2c979b29a3026b2ff05f2502251e53
biyue111 <biyue111@126.com>
2019-11-08 23:33:12 +0800

    up to date

--- DownlinkEnv.py
+++ DownlinkEnv.py
@@ -1,39 +1,62 @@
 import configs as configs
 import numpy as np
+from utils.utilFunc import *
 
 
 class DownlinkEnv:
     def __init__(self):
         self.user_positions = configs.USER_POSITIONS
         self.jammer_position = configs.JAMMER_POSITION
+        self.position_range = configs.POSITION_RANGE
         self.channel_num = configs.CHANNEL_NUM
         self.user_num = configs.USER_NUM
+        self.bs_max_power = configs.BS_MAX_POWER
+        self.max_user_datarate = self.get_max_datarate()
+        print("Max_user_datarate: ", self.max_user_datarate)
         self.sinr_threshold = configs.SINR_THRESHOLD  # if higher than that, this channel is occupied by jammer
 
     def get_init_state(self):
-        s = np.zeros(4 * self.user_num)
-        # Write user's position (x, y), channel chosen and data rate to the state
-        for i in range(self. user_num):
-            s[i*4] = self.user_positions[i][0]
-            s[i*4 + 1] = self.user_positions[i][1]
-            s[i * 4 + 2] = self.channel_num - 1
+        ini_jammer_ch = np.zeros(self.channel_num)
+        r = np.zeros(self.user_num)
+        ini_ch = [(self.channel_num - 1) for _ in range(self.user_num)]
+        s = self.generate_state(ini_jammer_ch, r, ini_ch)
         return s
 
-    def generate_state(self, ch, r):
-        s = np.zeros(4 * self.user_num)
+    def get_max_datarate(self):
+        max_sinr = 0
+        max_datarate = 0
+        for i in range(self.user_num):
+            sinr, datarate = calculate_datarate(self.bs_max_power, jammer_power=0.0,
+                                                user_num_in_channel=1.0, user_id=i)
+            max_datarate = max(max_datarate, datarate)
+            max_sinr = max(max_sinr, sinr)
+        return max_datarate
+
+    def generate_state(self, jammer_power_allocation, ch, r):
+        s = np.zeros(self.channel_num + 4 * self.user_num)
+        # Write jammer jammed channel
+        i = 0
+        for j in range(self.channel_num):
+            s[i] = jammer_power_allocation[j] / configs.JAMMER_POWER
+            i += 1
         # Write user's position (x, y), channel chosen and data rate to the state
-        for i in range(self. user_num):
-            s[i*4] = self.user_positions[i][0]
-            s[i*4 + 1] = self.user_positions[i][1]
-            s[i*4 + 2] = ch[i]
-            s[i*4 + 3] = r[i]
+        for j in range(self.user_num):
+            s[i] = self.user_positions[j][0] / self.position_range
+            i += 1
+            s[i] = self.user_positions[j][1] / self.position_range
+            i += 1
+            s[i] = channel_to_raw_channel(int(ch[j]))
+            i += 1
+            s[i] = r[j] / self.max_user_datarate
+            i += 1
         return s
 
     def calculate_bs_reward(self, bs_power_allocation, user_channel_ls, jammer_power_allocation):
         user_num_ls = np.zeros(self.channel_num)
         datarate_ls = np.zeros(self.user_num)
         sinr_ls = np.zeros(self.user_num)
-        jammed_reward_offset = -50
+        jammed_penalty = -5
+        min_reward = -2
         congested_user_reward_offset = 0
         # channel_availability[i][j] = 1: channel i is available for user j
         channel_availability = np.ones((self.channel_num, self.user_num))
@@ -54,7 +77,7 @@ class DownlinkEnv:
 
         # Calculate data rate (if found a a jammed user or having idle channel, no nned to calculate)
         for i in range(self.user_num):
-            sinr_ls[i], datarate_ls[i] = configs.calculate_datarate(bs_power_allocation[user_channel_ls[i]],
+            sinr_ls[i], datarate_ls[i] = calculate_datarate(bs_power_allocation[user_channel_ls[i]],
                                                         jammer_power_allocation[user_channel_ls[i]],
                                                         user_num_ls[user_channel_ls[i]], i)
 
@@ -62,14 +85,15 @@ class DownlinkEnv:
         jammed_flag = 0
         for i in range(self.user_num):
             if sinr_ls[i] < self.sinr_threshold and jammer_power_allocation[user_channel_ls[i]] > 0:
-                jammed_flag = 1
+                jammed_flag += 1
 
         reward = 0
-        if jammed_flag:
-            reward = jammed_reward_offset
+        if jammed_flag > 0:
+            reward = jammed_penalty * jammed_flag
         # elif congested_user_num > 0:
         #     reward = congested_user_reward_offset - congested_user_num
         reward += sum(datarate_ls)  # TODO: fairness
+        reward = max(min_reward, reward)
         # print("data rate list:" + str(datarate_ls))
 
         return jammed_flag, reward, datarate_ls
@@ -92,6 +116,6 @@ class DownlinkEnv:
         user_channel_ls = bs_action_ls[1]
 
         jammed_flag, reward, datarate_ls = self.calculate_bs_reward(bs_power_allocation, user_channel_ls, jammer_power_allocation)
-        s_ = self.generate_state(user_channel_ls, datarate_ls)
+        s_ = self.generate_state(jammer_power_allocation, user_channel_ls, datarate_ls)
 
         return jammed_flag, reward, s_
--- configs.py
+++ configs.py
@@ -3,18 +3,21 @@ import math
 
 # General configs
 CHANNEL_NUM = 3
-UPDATE_NUM = 1500
+UPDATE_NUM = 1000
 SINR_THRESHOLD = 0.5
+RAW_CHANNEL_STEP = 2.0 / (CHANNEL_NUM * 1.0)
+RAW_CHANNEL_LIST = np.arange(-1.0 + 0.5 * RAW_CHANNEL_STEP, 1.0, RAW_CHANNEL_STEP)
 
 # BS  and users common configs
 USER_NUM = 3
 USER_POSITIONS = np.array([[1.0, 1.0], [-1.0, 1.0], [1.0, -1.0], [-1.0, -1.0]])
+POSITION_RANGE = 1.0
 BATCH_SIZE = 499
 BS_MAX_POWER = 10
 VIRTUAL_ACTION_STEP = 0.2
 
 # Jammer common configs
-JAMMER_POWER = 10.0
+JAMMER_POWER = 30.0
 JAMMER_POSITION = [0.0, 0.0]
 
 # Channel condition configs
